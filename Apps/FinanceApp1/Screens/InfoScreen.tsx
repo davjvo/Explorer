82124a994d5992f3b18a7f2c6a24d2ab4728306d
Davmi Valdez <davjvo.dev@gmail.com>
2022-03-18 17:38:54 -0400

    init new screen

---
+++ Apps/FinanceApp1/Screens/InfoScreen.tsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const InfoScreen = () =>{
+    return (
+        <View><Text>asdasd</Text></View>
+    );
+}
+
+export default InfoScreen;
